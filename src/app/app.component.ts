87f2fa6f2e9ba4f61a607202fe41985bc0f6e8e8
GoodpastureR <eggboy06@gmail.com>
2024-04-03 10:48:19 -0600

    Tutorial branch: Creation, based on tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.less'
+  imports: [],
+  template: `
+    <h1>Default</h1>
+  `,
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'My-Website';
+  title = 'default';
 }
