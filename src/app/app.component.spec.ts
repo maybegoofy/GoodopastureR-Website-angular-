ab7779a466fb992d255cd1479f783cd36d298131
GoodpastureR <eggboy06@gmail.com>
2024-04-17 10:44:57 -0600

    moved from my-angular17-site here, to github

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'My-Website' title`, () => {
+  it(`should have the 'my-angular17-site' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('My-Website');
+    expect(app.title).toEqual('my-angular17-site');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, My-Website');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, my-angular17-site');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { AboutComponent } from './components/about/about.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { PricingComponent } from './components/pricing/pricing.component';
+import { ReferencesComponent } from './components/references/references.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { NaviComponent } from './components/navi/navi.component';
+import { CertsComponent } from './components/certs/certs.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, AboutComponent, ClientsComponent, PricingComponent, ReferencesComponent, GalleryComponent, NaviComponent, CertsComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.less'
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'My-Website';
+  title = 'my-angular17-site';
 }
